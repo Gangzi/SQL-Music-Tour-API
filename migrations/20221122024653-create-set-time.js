6d617cba1bdeef71d42d622b00a20e646a3a9ae2
Gang Cao <gangcao@Gangs-MacBook-Pro.local>
2022-11-23 19:25:48 -0500

    add associate

--- migrations/20221122024653-create-set-time.js
+++ migrations/20221122024653-create-set-time.js
@@ -1,43 +1,36 @@
-'use strict';
-/** @type {import('sequelize-cli').Migration} */
+'use strict'
 module.exports = {
-  async up(queryInterface, Sequelize) {
+  up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('set_times', {
-      id: {
+      set_time_id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      set_time_id: {
-        type: Sequelize.INTEGER
-      },
       event_id: {
-        type: Sequelize.SMALLINT
-      },
-      band_id: {
-        type: Sequelize.SMALLINT
+        type: Sequelize.SMALLINT,
+        allowNull: false
       },
       stage_id: {
-        type: Sequelize.SMALLINT
+        type: Sequelize.SMALLINT,
+        allowNull: false
+      },
+      band_id: {
+        type: Sequelize.SMALLINT,
+        allowNull: false
       },
       start_time: {
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        allowNull: false
       },
       end_time: {
-        type: Sequelize.DATE
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        allowNull: false
       }
-    });
+    })
   },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('set_times');
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('set_times')
   }
-};
+}
--- models/stage.js
+++ models/stage.js
@@ -1,24 +1,44 @@
-'use strict';
+'use strict'
 const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class stage extends Model {
+  class Stage extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
+    static associate({ Event, StageEvent, SetTime }) {
+      // events 
+      Stage.belongsToMany(Event, {
+        foreignKey: "stage_id",
+        as: "events",
+        through: StageEvent
+      })
+
+      // set times 
+      Stage.hasMany(SetTime, {
+        foreignKey: "stage_id",
+        as: "set_times"
+      })
     }
   }
-  stage.init({
-    stage_id: DataTypes.INTEGER,
-    stage_name: DataTypes.STRING
+  Stage.init({
+    stage_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    stage_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   }, {
     sequelize,
-    modelName: 'stage',
-  });
-  return stage;
-};
+    modelName: 'Stage',
+    tableName: 'stages',
+    timestamps: false
+  })
+  return Stage
+}
